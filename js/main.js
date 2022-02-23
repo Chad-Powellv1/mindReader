a4cfd2e1d9072130d1cfe59a7f35a96b78dcce63
Chad Powell <chad.powell@student.awesomeinc.org>
2022-02-23 21:23:10 +0000

    update main.js and index.html

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let head = document.getElementById('header');
+let help = document.getElementById('helper');
+let nextButton = document.getElementById('nextButton');
+let navButton = document.getElementById('navButton');
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '~'];
+let resultSymbol = null;
+let view = 0;
+
+
+// create function to loop through symbols to create 99 possible results
+   
+
+// create function to update state
+const updateState = function() {
+    view++;
+    updateView();
+}
+
+// create function to reset view
+const resetView = function() {
+    if (view === 0) {
+        view++;
+    } else {
+        view = 0;
+    }
+    updateView();
+}
+
+// create function to update view
+const updateView = function() {
+
+}
